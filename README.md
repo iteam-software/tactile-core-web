79660b51ce0723b8682aa20a7544f6157ffbbb3b
Matt Lynam <mlynam@iteamnm.com>
2017-07-07 16:58:09 -0600

    feat: setup project
    - create core files/folders
    - configure build
    - configure dev environment linting
    - add readme
    - add license

---
+++ README.md
@@ -0,0 +1,7 @@
+# TACTILE*web*
+## Game Engine Core
+
+Welcome to the game engine core library!!
+
+# Authors
+[@mlynam](https://github.com/mlynam)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tactile-core.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2017'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')],
+          },
+        },
+      },
+    ],
+  },
+};
