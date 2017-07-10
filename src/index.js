39392625adfe8151191a6b6e5b4a9c8682b7c37a
Matt Lynam <mlynam@iteamnm.com>
2017-07-10 10:10:35 -0600

    feat: export Timer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 import {Engine} from './engine';
 import {System} from './system';
+import {Timer} from './timer';
 
 const Tactile = {
   Engine,
   System,
+  Timer,
 };
 
 export default Tactile;
