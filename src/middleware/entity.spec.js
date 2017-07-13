d25d035b76499fcf24a67a699200c47e32783832
Matt Lynam <mlynam@iteamnm.com>
2017-07-13 16:54:24 -0600

    test: complete entity middleware tests

---
+++ src/middleware/entity.spec.js
@@ -0,0 +1,26 @@
+
+import {EntityMiddleware} from './entity';
+
+it('should call next if the action is not SpawnEntity type', (done) => {
+  const action = {type: 'NotSpawnEntity', a: 'b'};
+  const next = jest.fn((action) => {
+    expect(action).toEqual(action);
+    done();
+  });
+  EntityMiddleware({})(next)(action);
+  expect(next).toHaveBeenCalled();
+});
+
+it('should dispatch an Entity/Create when it handles spawn', (done) => {
+  const store = {dispatch: jest.fn((action) => action)};
+  const next = jest.fn((action) => {
+    expect(action.id).toBeTruthy();
+    expect(action.systems).toEqual(['TestSystem']);
+    expect(action.type).toBe('Entity/Create');
+    done();
+  });
+  const action = {type: 'SpawnEntity', systems: ['TestSystem']};
+  EntityMiddleware(store)(next)(action);
+  expect(store.dispatch).toHaveBeenCalled();
+  expect(next).toHaveBeenCalled();
+});
